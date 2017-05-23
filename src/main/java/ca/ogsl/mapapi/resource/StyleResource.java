efd58895b5ae8567e98fabb576f73bf216343e86
desjardinsa <desjardinsa@ogsl.ca>
2017-05-23 14:04:20 -0400

    Implémenter resource StyleResource
    Implémenté à l'aide du StyleService et GenericDao

--- src/main/java/ca/ogsl/mapapi/resource/StyleResource.java
+++ src/main/java/ca/ogsl/mapapi/resource/StyleResource.java
@@ -1,4 +1,30 @@
 package ca.ogsl.mapapi.resource;
 
+import ca.ogsl.mapapi.errorhandling.AppException;
+import ca.ogsl.mapapi.models.Style;
+import ca.ogsl.mapapi.services.StyleService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("styles")
+@Produces(MediaType.APPLICATION_JSON)
 public class StyleResource {
+
+    StyleService styleService = new StyleService();
+
+    @GET
+    public Response listStyles(@QueryParam("lang") String lang) throws AppException {
+        List<Style> styleList = this.styleService.listStyles(lang);
+        return Response.status(200).entity(styleList).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getStyleForId(@QueryParam("lang") String lang, @PathParam("id") Integer id) throws AppException{
+        Style style= this.styleService.getStyleForId(lang,id);
+        return Response.status(200).entity(style).build();
+    }
 }
